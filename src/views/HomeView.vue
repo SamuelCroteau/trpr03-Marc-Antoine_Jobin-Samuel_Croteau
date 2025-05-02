86a3a2225b2590707ba57b25ac48e157c7c7adf9
Marc-AntoineJobin <2356735@csfoy.ca>
2025-05-02 13:47:56 -0400

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    <h1>Accueil</h1>
+    <p>Lead dev login: test@test.com et mdp</p>
+  </div>
+</template>
+
+<style scoped></style>
